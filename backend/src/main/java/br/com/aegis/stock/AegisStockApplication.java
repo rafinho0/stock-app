713b7a2448857931871a599cd5fd0d38610422ea
rafinho0 <rafa06osilva@gmail.com>
2025-10-20 14:30:15 -0300

    Project Created

---
+++ backend/src/main/java/br/com/aegis/stock/AegisStockApplication.java
@@ -0,0 +1,13 @@
+package br.com.aegis.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AegisStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AegisStockApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/aegis/stock/AegisStockApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aegis.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AegisStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
